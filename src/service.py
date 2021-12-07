9a38f1997c7cd0d2a0ccd6085e7175124f94854e
Alexey Anisimov <alan81tablet@gmail.com>
2021-12-07 19:23:28 +1100

    Refactoring.

--- src/service.py
+++ src/service.py
@@ -23,21 +23,22 @@ fetch_timedelata = timedelta(minutes = 5)
 sense = SenseHat()
 owm = OWM(owm_api_key)
 mgr = owm.weather_manager()
-weather_at_location = None
-weather_at_location_time = None
 
 # Methods
 def show_message(text_string, scroll_speed=.1, text_colour=[255, 255, 255], back_colour=[0, 0, 0]):
+    # Scroll message on the SenseHat screen
     sense.set_rotation(180)
     sense.show_message(text_string, scroll_speed, text_colour, back_colour)
 
 def up_pressed(InputEvent):
+    # Handle SenseHat joystic up press
     if InputEvent.action == 'released':
         global sleep_time
         sleep_time = sleep_time + 1
         show_message(str(sleep_time))
 
 def down_pressed(InputEvent):
+    # Handle SenseHat joystic down press
     if InputEvent.action == 'released':
         global sleep_time
         if sleep_time > 1:
@@ -45,62 +46,77 @@ def down_pressed(InputEvent):
         show_message(str(sleep_time))
 
 def fetch_weather():
-    global weather_at_location
-    global weather_at_location_time
-
+    # Fetch weather from open weather map
     observation = mgr.weather_at_id(location_id)
     weather_at_location = observation.weather
     weather_at_location_time = datetime.now()
 
+    return weather_at_location, weather_at_location_time
+
+def take_readings():
+    # Take readings from the Sense hat sensors
+    t = sense.get_temperature()
+    p = sense.get_pressure()
+    h = sense.get_humidity()
+
+    # Round the values
+    t = round(t, 2)
+    p = int(round(p))
+    h = int(round(h))
+    return t,p,h
+
+def insert_entry(t, p, h, weather_at_location, col):
+    # Insert a new entry into the cluster
+    indoor = {}
+    indoor['t'] = t
+    indoor['p'] = p
+    indoor['h'] = h
+
+    outdoor = {}
+    # {'temp': 26.19, 'temp_max': 28.31, 'temp_min': 23.3, 'feels_like': 26.19, 'temp_kf': None}
+    outdoor['t'] = weather_at_location.temperature('celsius')['temp']
+    # {'press': 1009, 'sea_level': None}
+    outdoor['p'] = weather_at_location.barometric_pressure()['press']
+    outdoor['h'] = weather_at_location.humidity
+
+    entry = {}
+    entry['time'] = datetime.now()
+    entry['indoor'] = indoor
+    entry['outdoor'] = outdoor
+
+    col.insert_one(entry)
+
 # Main implementation
 try:
 
+    # Get climate_tracking collection
     client = MongoClient(connection_string)
     db = client.climate_db
     col = db.climate_tracking
 
+    # Init SenseHat joystic handlers
     # Inverting up and down because of the PI board location at my desk
     sense.stick.direction_up = down_pressed
     sense.stick.direction_down = up_pressed
     sense.stick.direction_middle = sense.clear
 
-    fetch_weather()
+    # Get initial weather information for the location
+    weather_at_location, weather_at_location_time = fetch_weather()
 
+    show_message('Starting...')
+    
     while True:
-        # Take readings from the Sense hat sensors
-        t = sense.get_temperature()
-        p = sense.get_pressure()
-        h = sense.get_humidity()
-
-        # Round the values
-        t = round(t, 2)
-        p = int(round(p))
-        h = int(round(h))
+        # Take readings from SenseHat
+        t, p, h = take_readings()
 
         # Fetch outdoor readings
         if datetime.now() > weather_at_location_time + fetch_timedelata:
-            fetch_weather()
+            weather_at_location, weather_at_location_time = fetch_weather()
 
-        # Insert entry
-        indoor = {}
-        indoor['t'] = t
-        indoor['p'] = p
-        indoor['h'] = h
+        # Insert entry into the cluster
+        insert_entry(t, p, h, weather_at_location, col)
 
-        outdoor = {}
-        # {'temp': 26.19, 'temp_max': 28.31, 'temp_min': 23.3, 'feels_like': 26.19, 'temp_kf': None}
-        outdoor['t'] = weather_at_location.temperature('celsius')['temp']
-        # {'press': 1009, 'sea_level': None}
-        outdoor['p'] = weather_at_location.barometric_pressure()['press']
-        outdoor['h'] = weather_at_location.humidity
-
-        entry = {}
-        entry['time'] = datetime.now()
-        entry['indoor'] = indoor
-        entry['outdoor'] = outdoor
-
-        col.insert_one(entry)
-
+        # Wait for the next beat
         sleep(sleep_time)
 
 finally:
